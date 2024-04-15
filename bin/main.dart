a41c08887ff85be564f721d677b17015d78ddbc4
LG <34185919+projektorius96@users.noreply.github.com>
2024-04-15 19:18:20 +0100

    a small code revision

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,6 @@
 import 'dart:io';
 import 'package:structio/export.dart';
 
-String genWhitespace(int n,
-    {String currentValue = "\xa0", String prefix = "", String suffix = ""}) {
-  String result = prefix;
-  for (int i = 0; i < n; i++) {
-    result += currentValue;
-  }
-  result += suffix;
-  return result;
-}
-
 List<FileSystemEntity> rootDir = [];
 List<FileSystemEntity> subrootDir = [];
 Future<bool> processStream(Stream<dynamic> dirent) async {
@@ -26,34 +16,34 @@ Future<bool> processStream(Stream<dynamic> dirent) async {
 }
 
 void main(List<String> args) async {
+  final sep = Platform.pathSeparator;
   Stream<FileSystemEntity> Dirent = Directory('.').list(recursive: true);
-  List<String> argv = Directory('.')
-      .absolute
-      .path
-      .split(Platform.pathSeparator)
-      .reversed
-      .toList();
-  final rootNamespace = "${argv[1]}${Platform.pathSeparator}";
+  List<String> argv = Directory('.').absolute.path.split(sep).reversed.toList();
+  final rootNamespace = "${argv[1]}$sep";
   await processStream(Dirent).then((done) async {
     var count = 0;
-    rootDir.forEach((element) {
+    rootDir.toList().forEach((FileSystemEntity dir) {
+      /// print [rootNamespace] only once;
       if (count == 0) {
         print(rootNamespace);
         count++;
       }
-      var splitResult = [...element.path.split(Platform.pathSeparator)].shift();
-      splitResult.length > 1
-          ? false
-          : print("${genWhitespace(rootNamespace.length-1)}${Platform.pathSeparator}${splitResult[0]}");
+      var splitResult = [...dir.path.split(sep)].shift();
+      if (splitResult.length > 1) {
+        print(
+            "${getWhitespaceDepth(rootNamespace.length - 1)}$sep${splitResult[0]}"
+        );
+      }
     });
   });
-  subrootDir.forEach((element) {
-    print("${element.path}${Platform.pathSeparator}");
+  subrootDir.toList().forEach((FileSystemEntity dir) {
+    print("${dir.path}$sep");
     /* print(element is Directory); */ // DEV_NOTE # scan each directory and print only its files
-    final subdirFiles = Directory(element.path).listSync();
-    subdirFiles.forEach((element) {
+    final subdirFiles = Directory(dir.path).listSync();
+    subdirFiles.toList().forEach((element) {
       if (element is File) {
-        print("${genWhitespace(element.parent.path.length)}${element.path.replaceFirst(element.parent.path, "")}");
+        print(
+            "${getWhitespaceDepth(element.parent.path.length)}${element.path.replaceFirst(element.parent.path, "")}");
       }
     });
   });
--- lib/src/structio_base.dart
+++ lib/src/structio_base.dart
@@ -4,19 +4,32 @@ extension List$Extension on List {
     this.removeLast();
     return this;
   }
+
   /// DEV_NOTE # alternatively use [Queue.removeFirst]
   List shift() {
     this.removeAt(0);
     return this;
   }
+
   /// DEV_NOTE # alternatively use [Queue.addFirst]
   List unshift(V) {
     this.insert(0, V);
     return this;
   }
+
   /// DEV_NOTE # alternatively use [Queue.addLast]
   List push(V) {
     this.add(V);
     return this;
   }
+}
+
+String getWhitespaceDepth(int n,
+    {String currentValue = "\xa0", String prefix = "", String suffix = ""}) {
+  String result = prefix;
+  for (int i = 0; i < n; i++) {
+    result += currentValue;
+  }
+  result += suffix;
+  return result;
 }
